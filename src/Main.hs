502e9fb19a1017176da765bcc1186bac454110b8
Evan Relf <evan@evanrelf.com>
2020-04-18 16:30:48 -0700

    Create basic `Task` type

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import Prelude hiding (id)
+
+
+data Task = Task
+  { id :: Int
+  , description :: Text
+  , status :: Status
+  } deriving Show
+
+
+data Status = Open | Closed
+  deriving Show
+
+
 main :: IO ()
-main = putTextLn "Hello world"
+main = do
+  print Task{id = 1, description = "Do the dishes", status = Open}
