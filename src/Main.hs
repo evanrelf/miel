dd51c496e8017b5dc9694d8cf342f0d0fe456ab9
Evan Relf <evan@evanrelf.com>
2020-04-18 17:32:44 -0700

    Create basic command-line options parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import qualified Options.Applicative as OA
 import Prelude hiding (id)
 
 
@@ -14,6 +15,25 @@ data Status = Open | Closed
   deriving Show
 
 
+data Options
+  = Add Text
+  deriving Show
+
+
+parseAdd :: OA.Parser Text
+parseAdd = unwords <$> some (OA.strArgument (OA.metavar "DESCRIPTION"))
+
+
+parseOptions :: OA.Parser Options
+parseOptions = OA.hsubparser $ mconcat
+  [ OA.command "add" (OA.info (Add <$> parseAdd) mempty)
+  ]
+
+
+getOptions :: IO Options
+getOptions = OA.execParser (OA.info (OA.helper <*> parseOptions) mempty)
+
+
 main :: IO ()
 main = do
-  print Task{id = 1, description = "Do the dishes", status = Open}
+  getOptions >>= print
