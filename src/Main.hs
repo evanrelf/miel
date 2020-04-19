5c0b9f641416b8a7817bc1031eaf208494f1cba8
Evan Relf <evan@evanrelf.com>
2020-04-18 22:30:14 -0700

    Test out `selda`

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,15 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedLabels #-}
 
 module Main (main) where
 
-import qualified Database.SQLite.Simple as SQLite
-import qualified Database.SQLite.Simple.FromField as SQLite
-import qualified Database.SQLite.Simple.Ok as SQLite
-import Database.SQLite.Simple.QQ (sql)
-import qualified Database.SQLite.Simple.ToField as SQLite
+import qualified Database.Selda as Selda
+import Database.Selda (Attr ((:-)))
+import qualified Database.Selda.SQLite as Selda
 import qualified Options.Applicative as OA
 import qualified System.Random as Random
 import Prelude hiding (id)
@@ -23,38 +20,12 @@ data Task = Task
   , description :: Text
   , status :: Status
   } deriving stock (Generic, Show)
-
-
-instance SQLite.ToRow Task where
-  toRow Task{..} = SQLite.toRow (id, description, status)
-
-
-instance SQLite.FromRow Task where
-  fromRow = Task <$> SQLite.field <*> SQLite.field <*> SQLite.field
+    deriving anyclass Selda.SqlRow
 
 
 data Status = Open | Closed
-  deriving (Read, Show)
-
-
-instance SQLite.ToField Status where
-  toField = SQLite.toField . show @Text
-
-
-instance SQLite.FromField Status where
-  fromField f@(SQLite.fieldData -> SQLite.SQLText text) =
-    case readMaybe (toString text) of
-      Just t -> SQLite.Ok t
-      Nothing ->
-        SQLite.returnError
-          SQLite.ConversionFailed
-          f
-          ("Status must be Open or Closed, got " <> toString text)
-  fromField f =
-    SQLite.returnError
-      SQLite.ConversionFailed
-      f
-      "expecting a SQLText column type"
+  deriving stock (Generic, Enum, Bounded, Read, Show)
+  deriving anyclass Selda.SqlType
 
 
 data Options
@@ -76,25 +47,17 @@ getOptions :: IO Options
 getOptions = OA.execParser (OA.info (OA.helper <*> parseOptions) mempty)
 
 
+tasks :: Selda.Table Task
+tasks = Selda.table "tasks" [ #id :- Selda.primary ]
+
+
 main :: IO ()
 main = do
   Add description <- getOptions
   id <- Random.randomRIO (1, 999)
-  let task = Task{ id, description, status = Open }
-  conn <- SQLite.open "miel.db"
-  SQLite.execute_
-    conn
-    [sql|
-      CREATE TABLE IF NOT EXISTS tasks
-        ( id INTEGER PRIMARY KEY
-        , description TEXT
-        , status TEXT
-        )
-    |]
-  SQLite.execute
-    conn
-    [sql|INSERT INTO tasks (id, description, status) VALUES (?, ?, ?)|]
-    task
-  results <- SQLite.query_ @Task conn [sql|SELECT * FROM tasks|]
-  mapM_ print results
-  SQLite.close conn
+  let cliTask = Task{ id, description, status = Open }
+  Selda.withSQLite "miel.db" do
+    Selda.tryCreateTable tasks
+    Selda.insert_ tasks [ cliTask ]
+    results <- Selda.query (Selda.select tasks)
+    mapM_ print results
