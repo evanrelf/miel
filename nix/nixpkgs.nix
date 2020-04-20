5caa34c42f6866f139feb7b3b217447861a98ae6
Evan Relf <evan@evanrelf.com>
2020-04-19 20:35:34 -0700

    Bump Nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,11 +1,11 @@
 args:
 
 let
-  # nixos-20.03 on 2020-03-23
-  rev = "b2935fbeceaea0b64df4401545d7c8ea29102120";
+  # nixos-20.03 on 2020-04-19
+  rev = "a17e021b9486a7699593f1a48f99f1a96a0dfc0e";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "1z1631v7q2c8mavy7xnvfx0wz34zd49jqmjg66nk0qgsi605m3qp";
+    sha256 = "0d2vz17rcxy4h5fkwngzz3nmqnqgj3irkhz7hkbacgs03h69z8gh";
   };
 in
   import nixpkgs ({ config = {}; } // args)
